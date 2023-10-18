0f1130cc25a2e5ee7da24b355e8c1fc63da88149
Joy <77735525+JOY2OP@users.noreply.github.com>
2023-10-18 21:41:53 +0530

    Create Valid Anagram.js

---
+++ 1. Arrays & Hashing/Valid Anagram.js
@@ -0,0 +1,9 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+const isAnagram = function(s, t) {
+    return s.split('').sort().join('') === t.split('').sort().join('');
+};
+
+console.log(isAnagram("sab","bas")) // after sort= "abs"
+//true
